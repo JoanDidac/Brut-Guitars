3e0af2c91b05155cf2620a411e2977d595cbdb2d
Joan D <joandidachr@gmail.comm>
2026-03-02 02:18:33 +0100

    feat: Add smooth hover animations with inline SVGs for footer social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './Footer.css';
-import brandLogo from '../assets/brut_logo.png';
+import brandLogo from '../assets/devil-horns-svgrepo-com.svg';
 import brandLogoLarge from '../assets/Brut logo.PNG';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -69,7 +69,7 @@ export default function Footer({ onNavigate }) {
                 <div className="container">
                     <div className="footer__top">
                         <div className="footer__brand">
-                            <img src={brandLogo} alt="Brut Guitars Logo" className="footer__logo-img" />
+                            <img src={brandLogo} alt="Devil Horns Logo" className="footer__logo-img" />
                             <p className="footer__tagline">
                                 Handcrafted custom guitars, built with passion and precision in La Garriga, Catalonia.
                             </p>
@@ -88,9 +88,32 @@ export default function Footer({ onNavigate }) {
                         <div className="footer__links-group">
                             <h4 className="footer__links-title">Follow</h4>
                             <ul className="footer__links">
-                                <li><a href="#" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-                                <li><a href="#" target="_blank" rel="noopener noreferrer">YouTube</a></li>
-                                <li><a href="#" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+                                        <span className="social-text">Instagram</span>
+                                        <svg className="social-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                                            <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+                                        <span className="social-text">YouTube</span>
+                                        <svg className="social-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.5 12 3.5 12 3.5s-7.505 0-9.377.55a3.016 3.016 0 0 0-2.122 2.136C0 8.07 0 12 0 12s0 3.93.501 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.55 9.377.55 9.377.55s7.505 0 9.377-.55a3.016 3.016 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" target="_blank" rel="noopener noreferrer" className="footer__social-link">
+                                        <span className="social-text">Facebook</span>
+                                        <svg className="social-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                            <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+                                        </svg>
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     </div>
